d446537b42cf994d7ac7cdbcb625e99f4817fc7f
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2016-07-31 23:01:10 -0300

    Add manual test for verifying if test method was run

---
+++ test_xunit.py
@@ -0,0 +1,7 @@
+from wasrun import WasRun
+from testcase import TestCase
+
+test = WasRun()
+print test.wasRun
+test.testMethod()
+print test.wasRun
---
+++ wasrun.py
@@ -0,0 +1,7 @@
+class WasRun():
+
+    def __init__(self):
+        self.wasRun = None
+
+    def testMethod(self):
+        self.wasRun = 1
