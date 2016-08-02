197c5f5ac43a5546a99672334a24adfe2d5b5541
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2016-08-02 00:43:18 -0300

    Simplify test using setup

--- test_xunit.py
+++ test_xunit.py
@@ -4,39 +4,37 @@ from testcase import TestCase, TestResult, TestSuite
 class TestCaseTest(TestCase):
     """Test if the TestCase is working"""
 
+    def setup(self):
+        self.result = TestResult()
+
     def test_template_method(self):
         test = WasRun("test_method")
-        result = TestResult()
-        test.run(result)
+        test.run(self.result)
         assert(test.log == "setup test_method tear_down ")
 
     def test_result(self):
         test = WasRun("test_method")
-        result = TestResult()
-        test.run(result)
-        assert("1 run, 0 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
 
     def test_failed_result(self):
         test = WasRun("test_broken_method")
-        result = TestResult()
-        test.run(result)
-        assert("1 run, 1 failed" == result.summary())
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
 
     def test_result_formatting(self):
-        result = TestResult()
-        result.test_started()
-        result.test_failed()
-        assert("1 run, 1 failed" == result.summary())
+        self.result.test_started()
+        self.result.test_failed()
+        assert("1 run, 1 failed" == self.result.summary())
 
     def test_suite(self):
         suite = TestSuite()
         suite.add(WasRun("test_method"))
         suite.add(WasRun("test_broken_method"))
 
-        result = TestResult()
-        suite.run(result)
+        suite.run(self.result)
 
-        assert("2 run, 1 failed" == result.summary())
+        assert("2 run, 1 failed" == self.result.summary())
 
 suite = TestSuite()
 suite.add(TestCaseTest("test_template_method"))
